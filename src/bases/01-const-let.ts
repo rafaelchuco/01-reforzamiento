2c5ea7f44c97241ae709fbddd2aac39577cf434c
rafaelchuco <rafael.chuco@tecsup.edu.pe>
2025-08-31 04:08:05 -0500

    feat(basics): declare let/const variables and basic operations (class 13)

---
+++ src/bases/01-const-let.ts
@@ -0,0 +1,10 @@
+let firstname = "Rafael";
+let lastname = "Chuco";
+
+let diceNumber = 5;
+diceNumber = 3;
+
+const containsLetterR = firstname.includes("R");
+console.log("¿Contiene 'R'?:", containsLetterR);
+console.log("Número de dado:", diceNumber);
+console.log("Nombre completo:", firstname, lastname);
